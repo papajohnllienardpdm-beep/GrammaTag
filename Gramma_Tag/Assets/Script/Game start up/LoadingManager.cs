173f83e78bbc95684c7fc446778bf5fbea0c0ff4
lien <papajohnllienard.pdm@gmail.com>
2026-03-13 00:25:35 +0800

    loading scene

---
+++ Gramma_Tag/Assets/Script/Game start up/LoadingManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingManager : MonoBehaviour
+{
+
+    public Slider loadingBar;
+    public string nextScene = "MainMenu";
+
+    public float loadingSpeed = 1f; // ikaw ang magdikta ng bilis
+
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(nextScene);
+        operation.allowSceneActivation = false;
+
+        float progress = 0f;
+
+        while (!operation.isDone)
+        {
+            progress += Time.deltaTime * loadingSpeed;
+            loadingBar.value = Mathf.Clamp01(progress);
+
+            if (loadingBar.value >= 1f)
+            {
+                yield return new WaitForSeconds(0.5f);
+                operation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+}
