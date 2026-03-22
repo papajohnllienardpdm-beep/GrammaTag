a204c7737d274df8b9d7431c1e7c3b1ffc164d3d
lien <papajohnllienard.pdm@gmail.com>
2026-03-22 17:13:12 +0800

    video lesson

--- Gramma_Tag/Assets/Script/VideoLesson/VideoControls.cs
+++ Gramma_Tag/Assets/Script/VideoLesson/VideoControls.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -9,12 +10,41 @@ public class VideoControls : MonoBehaviour
     public VideoPlayer videoPlayer;
     public Slider slider;
 
+    [Header("Time UI")]
+    public TextMeshProUGUI timeText;
+
+    [Header("Play Button")]
+    public Image playButtonImage;   // 👈 button icon
+    public Sprite playIcon;         // ▶️
+    public Sprite pauseIcon;
+
+    void Start()
+    {
+        UpdateIcon();
+    }
+
     void Update()
     {
         if (videoPlayer.isPlaying && videoPlayer.length > 0)
         {
-            slider.value = (float)(videoPlayer.time / videoPlayer.length);
+            float currentTime = (float)videoPlayer.time;
+            float totalTime = (float)videoPlayer.length;
+
+            // update slider
+            slider.value = currentTime / totalTime;
+
+            // update text
+            timeText.text = FormatTime(currentTime) + " / " + FormatTime(totalTime);
         }
+    }
+
+    // 👉 FORMAT TIME (important)
+    string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+
+        return string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     public void PlayPause()
@@ -23,6 +53,16 @@ public class VideoControls : MonoBehaviour
             videoPlayer.Pause();
         else
             videoPlayer.Play();
+
+        UpdateIcon();
+    }
+
+    void UpdateIcon()
+    {
+        if (videoPlayer.isPlaying)
+            playButtonImage.sprite = pauseIcon;
+        else
+            playButtonImage.sprite = playIcon;
     }
 
     public void Seek()
