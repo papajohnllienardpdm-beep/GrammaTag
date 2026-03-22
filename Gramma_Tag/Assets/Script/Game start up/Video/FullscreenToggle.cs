048022a77e4a5aaa97d07703c8bd63e192127858
lien <papajohnllienard.pdm@gmail.com>
2026-03-22 21:45:52 +0800

    video lesson (main menu)

---
+++ Gramma_Tag/Assets/Script/Game start up/Video/FullscreenToggle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullscreenToggle : MonoBehaviour
+{
+    public RectTransform panel;
+    public GameObject navigationBar; // 👈 ADD THIS
+
+    private bool isFullscreen = false;
+
+    private Vector2 originalAnchorMin;
+    private Vector2 originalAnchorMax;
+    private Vector2 originalOffsetMin;
+    private Vector2 originalOffsetMax;
+
+    void Start()
+    {
+        originalAnchorMin = panel.anchorMin;
+        originalAnchorMax = panel.anchorMax;
+        originalOffsetMin = panel.offsetMin;
+        originalOffsetMax = panel.offsetMax;
+    }
+
+    public void Toggle()
+    {
+        if (!isFullscreen)
+        {
+            // 👉 FULLSCREEN
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+            panel.anchorMin = Vector2.zero;
+            panel.anchorMax = Vector2.one;
+            panel.offsetMin = Vector2.zero;
+            panel.offsetMax = Vector2.zero;
+
+            navigationBar.SetActive(false); // 🔥 HIDE
+        }
+        else
+        {
+            // 👉 BACK TO NORMAL
+            Screen.orientation = ScreenOrientation.Portrait;
+
+            panel.anchorMin = originalAnchorMin;
+            panel.anchorMax = originalAnchorMax;
+            panel.offsetMin = originalOffsetMin;
+            panel.offsetMax = originalOffsetMax;
+
+            navigationBar.SetActive(true); // 🔥 SHOW
+        }
+
+        isFullscreen = !isFullscreen;
+    }
+}
---
+++ Gramma_Tag/Assets/Script/Game start up/Video/VideoControlss.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Video;
+
+
+public class VideoControlss : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Button playPauseButton;
+    public Slider slider;
+    public TextMeshProUGUI timeText;
+
+    private bool isPrepared = false;
+    private bool isPlaying = false;
+
+    void Start()
+    {
+        videoPlayer.prepareCompleted += OnVideoPrepared;
+
+        videoPlayer.Prepare();
+
+        timeText.text = "00:00 / 00:00";
+    }
+
+    void OnVideoPrepared(VideoPlayer vp)
+    {
+        Debug.Log("VIDEO PREPARED ✅");
+
+        isPrepared = true;
+
+        slider.value = 0;
+
+        timeText.text = "00:00 / " + FormatTime(videoPlayer.length);
+    }
+
+    void Update()
+    {
+        if (!isPrepared) return;
+
+        if (videoPlayer.length > 0)
+        {
+            slider.value = (float)(videoPlayer.time / videoPlayer.length);
+
+            timeText.text = FormatTime(videoPlayer.time) + " / " + FormatTime(videoPlayer.length);
+        }
+    }
+
+    public void TogglePlayPause()
+    {
+        
+
+        if (!isPrepared) return;
+
+        if (!isPlaying)
+        {
+           
+            videoPlayer.Play();
+            isPlaying = true;
+        }
+        else
+        {
+            
+            videoPlayer.Pause();
+            isPlaying = false;
+        }
+    }
+
+    string FormatTime(double time)
+    {
+        int min = Mathf.FloorToInt((float)time / 60);
+        int sec = Mathf.FloorToInt((float)time % 60);
+        return min.ToString("00") + ":" + sec.ToString("00");
+    }
+
+    public void OnSliderChanged()
+    {
+        if (!isPrepared) return;
+
+        videoPlayer.time = slider.value * videoPlayer.length;
+    }
+}
