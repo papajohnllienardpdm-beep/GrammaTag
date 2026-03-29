2b4e3a6327d287c01304573ec1b5c8b35dd64b13
lien <papajohnllienard.pdm@gmail.com>
2026-03-30 00:38:33 +0800

    shop 80%

---
+++ Gramma_Tag/Assets/Script/Shop/ShopPopupManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopPopupManager : MonoBehaviour
+{
+    public static ShopPopupManager Instance;
+
+    [Header("Popup UI")]
+    public GameObject popup;
+    public Image itemIcon;
+    public TMP_Text confirmText;
+
+    private ShopItem currentItem;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ShowPopup(ShopItem item)
+    {
+        currentItem = item;
+
+        popup.SetActive(true);
+
+        // set icon
+        itemIcon.sprite = item.icon;
+
+        // set text
+        confirmText.text = "Buy " + item.amount + " " + item.itemName + "?";
+    }
+
+    public void ConfirmBuy()
+    {
+        Debug.Log("BOUGHT: " + currentItem.itemName);
+
+        popup.SetActive(false);
+    }
+
+    public void Cancel()
+    {
+        popup.SetActive(false);
+    }
+}
