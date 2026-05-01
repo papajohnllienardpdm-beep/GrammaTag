790503f3b753a13fb4208ff6419d342fc6d78318
lien <papajohnllienard.pdm@gmail.com>
2026-05-02 02:10:03 +0800

    - certificate UI

---
+++ Gramma_Tag/Assets/Script/Certficate/CertificateButtonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CertificateButtonController : MonoBehaviour
+{
+    [Header("SET IN INSPECTOR")]
+    public int moduleID;
+
+    public GameObject lockPanel;
+    public Button buttonCertificate;
+
+    IEnumerator Start()
+    {
+        // 🔥 wait until DB is ready
+        while (!DatabaseManager.Instance.IsDatabaseReady())
+        {
+            yield return null;
+        }
+
+        CheckCertificateStatus();
+    }
+
+    void CheckCertificateStatus()
+    {
+        int userID = DatabaseManager.Instance.GetUserID();
+
+        bool isPassed = DatabaseManager.Instance.IsSubtopicPassed(moduleID);
+
+        if (isPassed)
+        {
+            // ✅ UNLOCK
+            lockPanel.SetActive(false);
+            buttonCertificate.interactable = true;
+        }
+        else
+        {
+            // 🔒 LOCK
+            lockPanel.SetActive(true);
+            buttonCertificate.interactable = false;
+        }
+    }
+}
